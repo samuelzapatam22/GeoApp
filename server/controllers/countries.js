1c344bdf07b9308b76f4268f532c4dad07e1f4a8
Diego Barrera <barrerajd07@gmail.com>
2024-01-14 17:14:43 +0100

    add controller to server

---
+++ server/controllers/countries.js
@@ -0,0 +1,29 @@
+const Country = require('../models/country')
+
+const createCountry = async (req,res) => {
+     try {
+        const{code,name,capital,currency,languages,continent} = req.body
+
+         const arrayLanguages = languages.map((lang)=>({name: lang}))
+        const newCountry = new Country({
+            code,
+            name,
+            capital,
+            currency,
+            languages: arrayLanguages,
+            continent
+        })
+        console.log(newCountry)
+
+         const saveCountry = await newCountry.save()
+         res.status(200).json(saveCountry)
+      } catch (error) {
+         console.log(error)
+         res.status(500).json({message: "server error"})
+         
+         
+      }
+ }
+ module.exports={
+    createCountry,
+ }
