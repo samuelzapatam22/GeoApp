5e8f0813e5219eaf0f26256d995baf3f801f2893
estivedelgado16 <138074670+estivedelgado16@users.noreply.github.com>
2024-02-05 15:44:09 -0500

    modifying styles

--- client/src/components/sliderBar/SliderBar.jsx
+++ client/src/components/sliderBar/SliderBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './sliderBar.css'
+import Logo from '../../assets/logo.png'
 
 function SliderBar() {
   return (
     <div className='sliderBar'>
+        <img src={Logo} alt="" />
         <Link to="/"></Link>
-        <Link to="/create">Create Country</Link>
-        <Link to="/home">Home</Link>
+        <Link to="/create" >Create Country</Link>
+        <Link to="/home" >Home</Link>
     </div>
   )
 }
