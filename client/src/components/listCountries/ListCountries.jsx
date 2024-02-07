ce7d7552a149a49f3931cb6da3ee9d72db98f79d
Deiiivy <deivypr28@gmail.com>
2024-02-07 17:30:53 -0500

    add popup

--- client/src/components/listCountries/ListCountries.jsx
+++ client/src/components/listCountries/ListCountries.jsx
@@ -5,11 +5,9 @@ import SearchBar from "../searchBar/SearchBar.jsx";
 import './listCountries.css'
 import Swal from "sweetalert2";
 
-
 function ListCountries() {
   const [lists, setList] = useState([]);
-  const [details, SetDetails] = useState(false);
-  const [info, setInfo] = useState({});
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   useEffect(() => {
     const getCountry = async () => {
@@ -25,32 +23,38 @@ function ListCountries() {
     getCountry();
   }, []);
 
-  const handleCountryClick = () => {
-
-    lists.map((list) => (
-      <p>{list.currency}</p>
-    ))
-    SetDetails(false)
-    console.log('click');
+  const handleCountryClick = (country) => {
+    setSelectedCountry(country); 
   };
 
+  const handleCloseDetails = () => {
+    setSelectedCountry(null)
+  }
   return (
     <div className='slider-bar'>
       <SliderBar />
       <div className='container-ListCountries'>
         <SearchBar/>
         <div className='ListCountries'>
-        {lists.map((list) => (
-          <div key={list._id} className='ListCountriesItem' onClick={handleCountryClick}>
-            <img src={`https://flagsapi.com/${list.code}/flat/64.png`} />
-            <p>{list.name}</p>
-            <p>{list.continent?.name}</p>
-          </div>
-        ))}
+          {lists.map((list) => (
+            <div key={list._id} className='ListCountriesItem' onClick={() => handleCountryClick(list)}>
+              <img src={`https://flagsapi.com/${list.code}/flat/64.png`} alt={list.name} />
+              <p>{list.name}</p>
+              <p>{list.continent?.name}</p>
+            </div>
+          ))}
         </div>
       </div>
+      {selectedCountry && (
+        <div className="country-details">
+          <h2>{selectedCountry.name}</h2>
+          <p>Currency: {selectedCountry.currency}</p>
+          <button onClick={() => handleCloseDetails()}>cerrar</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default ListCountries;
+
