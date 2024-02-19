a0f690536e8f63929e2014db6e57217860d9be32
Deiiivy <deivypr28@gmail.com>
2024-02-18 19:39:55 -0500

    add popup continents

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,6 +1,13 @@
 import "./SearchBar.css";
+import { useState } from "react";
 
 const SearchBar = ({ handleSearch }) => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowPopup(!showPopup); 
+  };
+
   return (
     <div className="SearchBar">
       <input
@@ -10,9 +17,20 @@ const SearchBar = ({ handleSearch }) => {
         placeholder="País"
         onChange={handleSearch}
       />
-      <button className="Button">Buscar</button>
+      <button className="Button" onClick={handleButtonClick}>Buscar</button>
+      {showPopup && (
+        <div className="popup">
+          <div className="head-continent-popup">
+          <h3>filtrar por continentes</h3>
+          <div className="container-popup">
+            <img className="asia" src="https://thumbs.dreamstime.com/b/europa-continente-con-los-contornos-de-pa%C3%ADses-dibujo-vectorial-forma-del-plan-distrito-la-isla-norte-sobre-fondo-blanco-l%C3%ADnea-226176348.jpg" alt="" />
+          </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
 
 export default SearchBar;
